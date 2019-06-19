30716abc4cda3a79893ff53417657c530128c633
Fernando Mendes <fernando.mendes91@outlook.com>
2019-06-18 23:57:35 -0300

    Introducao ao Hibernate, mapeamento de classe com tabela do banco

---
+++ hibernate-tutorial/scripts/02-student-tracker.sql
@@ -0,0 +1,19 @@
+
+-- Script criado para o começo do projeto, para criar uma tabela de estudantes na base de dados.
+CREATE DATABASE  IF NOT EXISTS `hb_student_tracker`;
+USE `hb_student_tracker`;
+
+--
+-- Table structure for table `student`
+--
+
+DROP TABLE IF EXISTS `student`;
+
+CREATE TABLE `student` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
