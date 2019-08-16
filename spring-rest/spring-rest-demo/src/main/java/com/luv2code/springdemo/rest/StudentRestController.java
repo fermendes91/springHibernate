fdd41a301bfbfba826475eb7a1a79a730bdbd5eb
Fernando Mendes <fernando.mendes91@outlook.com>
2019-08-15 22:41:39 -0300

    Spring Rest
    Aprendendo a tratar exceções em serviços rest.

--- spring-rest/spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ spring-rest/spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,6 +43,11 @@ public class StudentRestController {
 	public Student getStudent(@PathVariable Integer studentId) {
 		
 		// just index into the list ... keep it simple for now
+		
+		// check the studentID against list size
+		if( (studentId >= students.size() || studentId < 0)) {
+			throw new StudentNotFoundException("Student id not found - " + studentId);
+		}
 		return students.get(studentId);
 	}
 }
